6f0243ff9ebc3634d9005273b2ac22bcf9ae9555
Dario Carlino <baires82@gmail.com>
2020-08-22 04:18:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.include?(cheese_types)
-    puts array
+    puts cheese_types
   else
     return
   end
