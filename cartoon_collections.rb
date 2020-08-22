a4c1e1635df5484b67d0adf579d64d64e3412af7
Dario Carlino <baires82@gmail.com>
2020-08-22 04:39:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,10 +27,8 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?[cheese_types]
-    array.each do |i|
-      array.detect {|i| i == cheese_types}
-    end
+  array.select do |i|
+    i
   end
 end
 
