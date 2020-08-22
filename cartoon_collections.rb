9635f22472fc6dbb9e6660dabc1ebc5befd9906b
Dario Carlino <baires82@gmail.com>
2020-08-22 03:10:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(dwarves)
   i = 0
   while i < dwarves.length
     dwarves.each_with_index do |name|
-      puts "/#{i}.*#{name}/"
+      puts "/#{i+1}.*#{name}/"
       i += 1
     end
   end
